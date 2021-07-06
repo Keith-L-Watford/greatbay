17c8f9f0592de2b3d1b27b3c1a79eb967b1776b6
Keith Watford <keith.l.watford@gmail.com>
2021-07-06 15:25:44 -0400

    added a category to the sql files

--- schema.sql
+++ schema.sql
@@ -7,6 +7,7 @@ USE greatbayDB;
 CREATE TABLE items (
     id INTEGER NOT NULL AUTO_INCREMENT,
     item_name VARCHAR (50) NOT NULL,
+    category VARCHAR (50) NOT NULL,
     starting_big INTEGER(10) DEFAULT 0,
     highest_bid INTEGER(10) DEFAULT 0,
     PRIMARY KEY (id)

d02cbbf94d2ad2873203a1dc685a4a8497d6c09b
Keith Watford <keith.l.watford@gmail.com>
2021-07-06 12:25:44 -0400

    trying to add dynamic choices for the user

--- greatBay.js
+++ greatBay.js
@@ -3,21 +3,48 @@ const inquirer = require('inquirer');
 require('dotenv').config()
 
 // mysql
-const mysql      = require('mysql');
+const mysql = require('mysql');
 const connection = mysql.createConnection({
-  host     : 'localhost',
-  port     : 3306,  
-  user     : process.env.DB_USER,
-  password : process.env.DB_PASS,
-  database : process.env.DB_NAME,
+    host: 'localhost',
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
 });
 
 const letsGo = () => {
-    inquirer.prompt({
+    inquirer.prompt([{
+        type: 'list',
+        message: 'Would you like to sell or buy an item?',
+        choices: ['Buy', 'Sell'],
+        name: 'buyOrSell',
+    }, ])
 
+    .then((data) => {
+        if (data.buyOrSell === 'Buy') {
+            weBuying()
+        } else {
+            weSelling()
+        }
     })
-connection.end()
+    // connection.end()
 }
+
+
+const weBuying = () => {
+    console.log('Here are our current listed itmes...\n');
+    connection.query('SELECT * FROM items', (err, res) => {
+        if (err) throw err;
+        console.table(res)
+
+
+        
+        inquirer.prompt([{
+            
+        }])
+        connection.end()
+    });
+};
 
 
 
@@ -25,4 +52,4 @@ connection.connect((err) => {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}`);
     letsGo();
-}) 
+})
